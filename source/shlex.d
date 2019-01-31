d076dcc2c29744dcfa07b074b266ecfe1e448383
Victor Porton <porton@narod.ru>
2019-01-31 02:26:39 +0200

    copyright notice in the source

---
+++ source/shlex.d
@@ -0,0 +1,23 @@
+module shlex;
+
+/*
+shlex, simple shell-like lexical analysis library
+Copyright (C) 2019  Victor Porton
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This code was a rewrite of a Python 3.7 module with the same name:
+Copyright © 2001-2019 Python Software Foundation; All Rights Reserved
+*/
+
