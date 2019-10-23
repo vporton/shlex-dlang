691d1dbe27937ab88e594689eca780a664167dee
Victor Porton <porton@narod.ru>
2019-10-23 15:13:41 +0300

    bug fix

--- shlex-main.d
+++ shlex-main.d
@@ -2,15 +2,19 @@ module shlex_main;
 
 import std.stdio;
 import std.typecons;
+import std.file;
+import std.utf;
+import std.array;
 import shlex;
 
+// TODO: byLine is inefficeint
 void main(string[] args)
 {
     if (args.length == 1)
-        _printTokens(*new Shlex(stdin));
+        _printTokens(*new Shlex(stdin.byLine.join));
     else {
         immutable filename = args[1];
         scope File file = File(filename, "r");
-        _printTokens(*new Shlex(file, Nullable!string(filename)));
+        _printTokens(*new Shlex(file.byLine.join, Nullable!string(filename)));
     }
 }
