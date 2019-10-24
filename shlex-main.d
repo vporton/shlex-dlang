7c1b391357781017639c54f0783d5efaa8f74dfc
Victor Porton <porton@narod.ru>
2019-10-24 11:39:25 +0300

    rewritten the main app

--- shlex-main.d
+++ shlex-main.d
@@ -5,17 +5,21 @@ import std.typecons;
 import std.file;
 import std.utf;
 import std.array;
+import std.algorithm;
 import shlex;
 
-// TODO: byLine is inefficient.
+// TODO: inefficient.
+auto readFile(File file) {
+    return file.byLine(Yes.keepTerminator).join;
+}
+
 void main(string[] args)
 {
     if (args.length == 1)
-        _printTokens(*new Shlex(stdin.byLine(Yes.keepTerminator).join));
+        _printTokens(*new Shlex(stdin.readFile));
     else {
         immutable filename = args[1];
         scope File file = File(filename, "r");
-        _printTokens(*new Shlex(file.byLine(Yes.keepTerminator).join,
-                                Nullable!string(filename)));
+        _printTokens(*new Shlex(file.readFile, nullable(filename)));
     }
 }
