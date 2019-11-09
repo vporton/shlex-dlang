931312f84fc082bfc2ab04b06d18c875c6d24458
Victor Porton <porton@narod.ru>
2019-11-09 23:32:28 +0200

    now uses dependency injection

--- shlex-main.d
+++ shlex-main.d
@@ -15,17 +15,20 @@ auto readFile(File file) {
 
 void main(string[] args)
 {
-    if (args.length == 1)
-        _printTokens(*new Shlex(stdin.readFile));
-    else {
+    auto provider = new ShlexProviderStream!(char[]).ShlexProvider;
+
+    alias ParamsType = ShlexProviderStream!(char[]).ShlexParams.WithDefaults;
+    ParamsType params;
+    if (args.length == 1) {
+        ParamsType p = {instream: stdin.readFile, infile: Nullable!string.init};
+        params = p;
+    } else {
         immutable filename = args[1];
         scope File file = File(filename, "r");
 
-        auto provider = new ShlexProviderStream!(char[]).ShlexProvider;
-        ShlexProviderStream!(char[]).ShlexParams.WithDefaults params = {instream: file.readFile, infile: nullable(filename)};
-        Shlex *shlex = provider.callWithDefaults(params);
-
-        //_printTokens(*new Shlex(file.readFile, nullable(filename)));
-        _printTokens(shlex);
+        ParamsType p = {instream: file.readFile, infile: nullable(filename)};
+        params = p;
     }
+    Shlex *shlex = provider.callWithDefaults(params);
+    _printTokens(*shlex);
 }
